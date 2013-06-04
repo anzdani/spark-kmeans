a76e99dc29dc4f3917b903b552bee98062987fef
Dani <annuzzi.daniele@gmail.com>
2013-06-04 23:58:22 +0200

    Updated Main method to support KMeans with Broadcast vars

--- src/main/scala/MultiSparkKmeans.scala
+++ src/main/scala/MultiSparkKmeans.scala
@@ -3,12 +3,15 @@ package main
 import spark._
 import spark.SparkContext._
 import spark.broadcast.Broadcast
+import spark.KryoSerializer
+import spark.KryoRegistrator
 
 import scala.io._
 import com.codahale.jerkson.Json._
 import scala.collection.JavaConversions._
 //import com.rockymadden.stringmetric
 //import com.rockymadden.stringmetric.similarity._
+import com.esotericsoftware.kryo._
 import scala.util.Random.nextInt
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
@@ -20,8 +23,16 @@ import main.support._
 import main.support.Support._
 import main.feature._
 
-object Main extends App {
+class MyRegistrator extends KryoRegistrator {
+  override def registerClasses(kryo: Kryo) {
+    kryo.register(classOf[Feature])
+    kryo.register(classOf[Elem])
+    kryo.register(classOf[Numeric])
+    kryo.register(classOf[Categorical])
+  }
+}
 
+object Main extends App {
   //Logger.getLogger("spark").setLevel(Level.WARN)
 
   if (args.length < 3) {
@@ -72,19 +83,23 @@ object MultiSparkKmeans {
     val convergeDist = parameters("convergeDist").asInstanceOf[Double]
     val maxIter = parameters("maxIter").asInstanceOf[Int]
     val numSamplesForMedoid = parameters("numSamplesForMedoid").asInstanceOf[Int]
-    val weights: Map[String, Double] = parameters("weights").asInstanceOf[java.util.LinkedHashMap[String, Double]].toMap
+    val weights : Map[String, Double] = parameters("weights").asInstanceOf[java.util.LinkedHashMap[String, Double]].toMap
 
     // Create a SparkContext Object to access the cluster
+    System.setProperty("spark.serializer", "spark.KryoSerializer") 
+    System.setProperty("spark.kryo.registrator", "main.MyRegistrator")
     val sc = new SparkContext(host, appName, System.getenv("SPARK_HOME"), List("./target/job.jar"))
 
     //  Create a Vectorial Space with defined distances and weights 
     val geometry = VSpace(weights.values.toList, numSamplesForMedoid)
-
+    val geoBC : Broadcast[VectorSpace[Elem]]  = sc.broadcast(geometry)
+    
     // Create a test object to verify whether number of features and weights match or not
     val test = ElemFormat.construct1("{}")
     require(geometry.weights.size == (test.terms.size + test.categs.size), "Error: wrong number of features in parameter.conf")
     require( (1.0 - geometry.weights.sum) < 0.001, "Error: sum of weights in parameter.conf must be 1" ) 
     require(test.categs.size > 0 || (test.categs.size == 0 && geometry.numSamplesForMedoid == 0), "Error: numSamplesForMedoid must be 0 if number of Categorical features is 0")
+    
     //  Input Step
     /*  RDD Creation:  Parse input file into a RDD  */
     /*  RDD Transformation:  Featurization of dataset  */
@@ -93,11 +108,11 @@ object MultiSparkKmeans {
       ElemFormat.construct1(line)
     })
 
-    if (DEBUG) {
-      println(Console.CYAN + "READ" + "-" * 100 + "\nRead " + pointsRaw.count() + " points." + Console.WHITE)
-      println(Console.CYAN + pointsRaw.collect().map(_.toString() + "\n").mkString)
-      println(Console.WHITE)
-    }
+    // if (DEBUG) {
+    //   println(Console.CYAN + "READ" + "-" * 100 + "\nRead " + pointsRaw.count() + " points." + Console.WHITE)
+    //   println(Console.CYAN + pointsRaw.collect().map(_.toString() + "\n").mkString)
+    //   println(Console.WHITE)
+    // }
 
     //  Normalize Step
     val elMax: Elem = findElem(pointsRaw, "max", Numeric.maxLimit)
@@ -105,12 +120,14 @@ object MultiSparkKmeans {
     val points = Support.scaleInput(pointsRaw, elMax, elMin).cache()
 
     //  RDD Action to set k random centroids from points
-    val centroids = points.takeSample(withReplacement = false, initialCentroids, seed = nextInt())
-    //val centroidsBC : Broadcast[Array[Elem]]  = sc.broadcast(centroids)
+    val centroids = points.takeSample(withReplacement = false, initialCentroids, seed = 42/*nextInt()*/)
+    val centroidsBC : Broadcast[Array[Elem]]  = sc.broadcast(centroids)
+    
     // Run the kmeans algorithm 
-    //val resultCentroids = KMeans(sc,points, centroidsBC, convergeDist, geometry)
-    val (resultCentroids, iter) = KMeans(points, centroids, convergeDist, 0, maxIter, geometry)
-    val result = scaleOutput(resultCentroids, elMax, elMin)
+    //val (resultCentroids, iter) = KMeans(sc, points, centroidsBC, convergeDist, 1, maxIter, geoBC)
+    val (resultCentroids, iter) = KMeansDistributed(sc, points, centroidsBC, convergeDist, 1, maxIter, geoBC)
+    
+    val result = resultCentroids.map(scaleElem(_, elMax, elMin))
 
     //Centroids output  
     println(Console.GREEN)
@@ -118,8 +135,8 @@ object MultiSparkKmeans {
     println("RESULT-CENTROIDS" + "-" * 100)
     println(result.map(_.toString() + "\n").mkString)
     println(Console.WHITE)
-    Evaluation(points,centroids,geometry)
+    
+    Evaluation(points,resultCentroids,geometry,elMax,elMin)
     result 
   }
-
 }
